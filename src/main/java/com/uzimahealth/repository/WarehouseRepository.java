082643d89e59d74154fed71d58b38a4f0fd84f43
Philip Ademba <philmaxsnr@gmail>
2025-11-01 15:00:47 +0300

    Add Warehouse model and repository - hierarchical warehouse management

---
+++ src/main/java/com/uzimahealth/repository/WarehouseRepository.java
@@ -0,0 +1,7 @@
+package com.uzimahealth.repository;
+
+import com.uzimahealth.stock.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+}
